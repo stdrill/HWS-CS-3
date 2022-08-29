ba55d87606f4cbf21709cd0e1f0a801dd8ee57a1
stdrill <tanya23.96@mail.ru>
2022-08-29 23:54:43 +0300

    task1 and gitignore added

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= n){
+    Console.Write($"{count * count * count} ");
+    count ++;
+}

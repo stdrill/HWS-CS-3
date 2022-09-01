003ed506af791438ba0c4e5861c305bb6d531d3c
stdrill <tanya23.96@mail.ru>
2022-09-01 22:46:45 +0300

    task7 added

---
+++ task7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+void FillArray(int[] arr){
+    for(int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(-9,10);
+}
+void PrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i]+" ");
+}
+int CountArray(int[] arr){
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i] > 0) count = count + arr[i]; 
+    }
+    return count;
+}
+
+int[] arr = new int[12];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+int count = CountArray(arr);
+Console.WriteLine($"Сумма положительных элементов: {count}");

40e96d36e2f15bf4c5a6fac110cbccd49353530b
stdrill <tanya23.96@mail.ru>
2022-09-01 21:52:01 +0300

    task6 added

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillArray(int[] arr){
+    for(int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(0,2);
+}
+void PrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i]+" ");
+}
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);

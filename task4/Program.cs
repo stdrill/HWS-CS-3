dc5a3ce9cf7a46f4df9940c6fd84d74ea3b866a9
stdrill <tanya23.96@mail.ru>
2022-09-01 20:52:37 +0300

    task4 added branch main

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int NumProduct (int num){
+    int prod = 1;
+    for(int i = 1; i < num; i++){
+        prod = prod * (i + 1);
+    }
+    return prod;
+}
+Console.WriteLine("Enter number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int prod = NumProduct(num);
+Console.WriteLine(prod);

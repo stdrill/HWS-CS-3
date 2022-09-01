e188e1487ffd17db68d301e4b851fa90f3ae0005
stdrill <tanya23.96@mail.ru>
2022-09-01 21:47:54 +0300

    task5 added

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+void FillArray(int[] arr){
+    for(int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(10,50);
+}
+void PrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i]+" ");
+}
+int[] Exponentiation(int[] arr){
+    for(int i = 0; i < arr.Length; i++){
+        if (arr[i] %2 ==0 ) arr[i] = arr[i] * arr[i] * arr[i];
+    } 
+    return arr;
+}
+int[] arr = new int[5];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+int[] expon = Exponentiation(arr);
+PrintArray(expon);
